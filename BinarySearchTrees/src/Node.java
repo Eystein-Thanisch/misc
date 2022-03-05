5409e31b59b7d6c64b5ec53afa313797c0b265e7
Eystein Thanisch <oyysch@googlemail.com>
2022-03-05 10:58:31 +0000

    Started implementation of a binary search tree - coded up node class

---
+++ BinarySearchTrees/src/Node.java
@@ -0,0 +1,19 @@
+public class Node {
+
+    int data;
+    Node leftChild;
+    Node rightChild;
+
+    public Node(int data) {
+        this.data = data;
+    }
+
+    public void setLeftChild(Node leftChild) {
+        this.leftChild = leftChild;
+    }
+
+    public void setRightChild(Node rightChild) {
+        this.leftChild = rightChild;
+    }
+
+}
